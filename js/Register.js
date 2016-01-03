7354c1dc97bc51463abcf6b48942570adddedec6
yue-b <yue@byuedeMBP.lan>
2016-01-03 21:49:12 +0800

    register

--- js/Register.js
+++ js/Register.js
@@ -1,13 +1,26 @@
-function Register(){
+function Register() {
 	console.log("test");
-	var username=$("#userName").val();
-	var phone=$("#phone").val();
-	console.log(username);
-	$.ajax({
-		url:"/BookStore/Register?userName="+username+"&phone="+phone+"&email="+email+"&password0="+password0+"&password1="+password1,
-		contentType:"text/json;charset=utf-8",
-		type:"POST",		
-		success:function(data){	
-		}
-	});
+	var userName = $("#userName").val();
+	var phone = $("#phone").val();
+	var email = $("#email1").val();
+	var password0 = $("#password0").val();
+	var password1 = $("#password1").val();
+	console.log("userName"+userName.length+"phone"+phone.length+"email"+email.length+"password0"+password0.length+"password1"+password1.length);
+	if (userName.length != 0 && phone.length != 0 && email.length != 0
+			&& password0.length != 0 && password1.length != 0) {
+		if(password0 == password1)
+			$.ajax({
+				url : "/BookStore/Register?userName=" + userName + "&phone="
+						+ phone + "&email=" + email + "&password0=" + password0
+						+ "&password1=" + password1,
+				contentType : "text/json;charset=utf-8",
+				type : "POST",
+				success : function(data) {
+				}
+			});
+		else
+			console.log("密码不一致");
+	} else
+		console.log("存在参数为空");
+
 }
