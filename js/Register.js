55ecfa7b74da68c4094b740abbcc464b9ccaf061
yue-b <yue@byuedeMBP.lan>
2016-01-03 20:09:38 +0800

    test

--- js/Register.js
+++ js/Register.js
@@ -1,10 +1,12 @@
 function Register(){
-	
+	console.log("test");
+	var username=$("#userName").val();
+	console.log(username);
 	$.ajax({
-		url:"/BookStore/Login?userName="+username+"&password="+password,
+		url:"/BookStore/Register?userName="+username+"&phone="+phone+"&email="+email+"&password0="+password0+"&password1="+password1,
 		contentType:"text/json;charset=utf-8",
 		type:"POST",		
 		success:function(data){	
 		}
-	})
+	});
 }
